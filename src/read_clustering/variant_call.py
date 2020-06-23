7233d5133a8378634adda449dfa3ab3d7c73deba
Andrew D Bailey IV <bailey.andrew4@gmail.com>
2020-06-23 12:42:26 -0700

    setup python and testing environment

---
+++ src/read_clustering/variant_call.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+"""Variant call file class handler"""
+########################################################################
+# File: variant_call.py
+#
+#
+# Author: Andrew Bailey, Shreya Mantripragada, Alejandra Duran, Abhay Padiyar
+# History: 06/23/20 Created
+########################################################################
+
+
+class VariantCall(object):
+    def __init__(self):
+        pass
