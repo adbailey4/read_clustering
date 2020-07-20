6290c7e801f67b4294e4ebc81379d7853283c301
aduranu <67295131+aduranu@users.noreply.github.com>
2020-07-20 11:27:56 -0500

    Added cython

--- setup.py
+++ setup.py
@@ -54,5 +54,6 @@ setup(
         'matplotlib>=3.2.2',
         'seaborn>=0.10.1',
         'scikit-learn>=0.23.1',
-        'hdbscan>=0.8.26']
+        'hdbscan>=0.8.26',
+        'Cython>=0.29.21']
 )
