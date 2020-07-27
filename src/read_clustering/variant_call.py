3788f28f93747c64929daebaae71d44efbdbb452
aduranu <alejandraduran.u@gmail.com>
2020-07-27 14:42:16 -0500

    Completed methods for clustering algorithms. Return fit(X), fit_predict(X) attributes

--- src/read_clustering/variant_call.py
+++ src/read_clustering/variant_call.py
@@ -9,25 +9,39 @@
 ########################################################################
 
 import os
+import math
+import scipy
+import hdbscan
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns; sns.set()
-plt.rcParams['figure.figsize'] = [12, 7]
 from collections import Counter
+from collections import defaultdict
+from itertools import cycle
+from scipy.sparse import csgraph
+from scipy.sparse.linalg import eigsh
+from scipy.spatial.distance import pdist, squareform
+from scipy.cluster import hierarchy
+from sklearn import metrics
+from sklearn.neighbors import NearestNeighbors
 from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
 from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
 from sklearn.mixture import GaussianMixture
-from itertools import combinations
-from collections import defaultdict
 from sklearn.cluster import AffinityPropagation
+from sklearn.cluster import AgglomerativeClustering
 from sklearn.cluster import DBSCAN
-from sklearn import metrics
-from itertools import cycle
-import hdbscan
-
+from sklearn.cluster import MeanShift
+from sklearn.cluster import SpectralClustering
+from sklearn.mixture import GaussianMixture as GMM
+from sklearn.cluster import estimate_bandwidth
+from sklearn.model_selection import train_test_split
+from yellowbrick.cluster import KElbowVisualizer  # needs installation
+from sklearn.metrics import silhouette_score
+import shapely.geometry as SG  # INSTALL
+from kneed import KneeLocator  # INSTALL
 
 
 class VariantCall(object):
@@ -311,15 +325,10 @@ class VariantCall(object):
         positions = []
         variants = []
         for i, row in data_2.iterrows():
-            x = positions.append(i)
-        return x
+            r = positions.append(i)
+        return r
 
-    def get_reads_covering_positions_data(self, positions):  # don't need variants anymore
-        """return dataframe with probabilities for the modified variants in the given variant_set for the given position
-        Params: positions: target positions as a list
-            variants: target variants as a list in corresponding order to the positions list
-        Returns: df_plot: data frame with the probabilities for each variant at each position with corresponding read id.
-            """
+    def get_reads_covering_positions_data(self, positions, plot=False):
         data = self.data[self.data['reference_index'].isin(positions)]
         plot_data = data.loc[:, ['read_id', 'reference_index', 'variants', 'prob1', 'prob2']]
         pos_n = len(positions)
@@ -330,123 +339,223 @@ class VariantCall(object):
         d = {}
         for pos in positions:
             d[str(pos)] = []
-
         for i in target_ids:
             r = (plot_data.loc[plot_data['read_id'] == i]).set_index('reference_index')
             for index, row in r.iterrows():
                 d[str(index)].append(r.at[index, 'prob2'])
-
         df_plot = pd.DataFrame(list(zip(target_ids)))
         df_plot.columns = ['read_id']
         for key in d:
             col_val = ''
             col_val += 'P' + ' ' + str(key)
             df_plot[col_val] = d[key]
+        if plot:
+            del df_plot['read_id']
         return df_plot
 
-    def plot_tSNE_reads_covering_positions_data(self, positions, clusters_n, clustering_algorithm):
+    ##Spectral clustering
+
+    def spectral_clusters(self, positions):
+        X = self.get_reads_covering_positions_data(positions, plot=True)
+        dists = squareform(pdist(X.values))  # ndarray
+        neighbor_n = int(math.sqrt(len(positions)))
+        knn_distances = np.sort(dists, axis=0)[neighbor_n]  # indexer is usually sqrt(number_of_data_points)
+        knn_distances = knn_distances[np.newaxis].T
+        local_scale = knn_distances.dot(knn_distances.T)
+        a_matrix = dists * dists
+        a_matrix = -a_matrix / local_scale
+        a_matrix[np.where(np.isnan(a_matrix))] = 0.0
+        a_matrix = np.exp(a_matrix)
+        np.fill_diagonal(a_matrix, 0)  # now have a_matrix
+        A = a_matrix
+        L = csgraph.laplacian(A, normed=True)
+        n_components = A.shape[0]
+        eigenvalues, eigenvectors = eigsh(L, k=n_components, which='LM', sigma=1.0, maxiter=500)
+        plt.title('Largest eigen values of input matrix')
+        plt.scatter(np.arange(len(eigenvalues)), eigenvalues)
+        plt.xlabel('K clusters')
+        plt.ylabel('Eigengap')
+        axes = plt.gca()
+        axes.set_xlim([-5, 80])
+        return plt.grid()
+
+    def spectral_affinity(self, positions, n_clusters):
+        X = self.get_reads_covering_positions_data(positions, plot=True)
+        spectral_model_rbf = SpectralClustering(n_clusters=n_clusters, affinity='rbf')  # with any value for n_clusters
+        labels_rbf = spectral_model_rbf.fit_predict(X)
+        spectral_model_nn = SpectralClustering(n_clusters=n_clusters, affinity='nearest_neighbors')
+        labels_nn = spectral_model_nn.fit_predict(X)
+        affinity = ['rbf', 'nearest_neighbors']
+        s_scores = [silhouette_score(X, labels_rbf), silhouette_score(X, labels_nn)]
+        plt.bar(affinity, s_scores)
+        plt.xlabel('Affinity')
+        plt.ylabel('Silhouette Score')
+        plt.title('Comparison of different Clustering Models')
+        return plt.show()
+
+    def spectral_clustering(self, positions, n_clusters=1, affinity=1, max_number_clusters=None, cluster_size=None,
+                            eps=None, min_samples=None, n_components=None):
+        X = self.get_reads_covering_positions_data(positions, plot=True)
+        return SpectralClustering(n_clusters=n_clusters, affinity=affinity).fit(X), SpectralClustering(
+            n_clusters=n_clusters, affinity=affinity).fit_predict(X)
+
+    ##mean shift
+
+    def mean_shift(self, positions, n_clusters=None, affinity=None, max_number_clusters=None, cluster_size=None,
+                   eps=None, min_samples=None, n_components=None):
+        X = self.get_reads_covering_positions_data(positions, plot=True)
+        quantile_data = X.to_numpy()
+        quantile_val = np.quantile(quantile_data, 0.5)
+        bandwidth = estimate_bandwidth(X, quantile=quantile_val)
+        ms = MeanShift(bandwidth=bandwidth)
+        mf = ms.fit(X)
+        print('Number of clusters found: ', len(mf.cluster_centers_))
+        return mf, ms.fit_predict(X)
+
+    ##K means
+
+    def k_means_clusters(self, positions, max_number_clusters):
+        X = self.get_reads_covering_positions_data(positions, plot=True)
+        model = KMeans()
+        visualizer = KElbowVisualizer(model, k=(1, max_number_clusters + 1))
+        v = visualizer.fit(X)
+        v.show()
+        return KMeans(n_clusters=v.elbow_value_).fit(X)
+
+    def k_means(self, positions, n_clusters=None, affinity=None, max_number_clusters=1, cluster_size=None,
+                eps=None, min_samples=None, n_components=None):
+        X = self.get_reads_covering_positions_data(positions, plot=True)
+        model = KMeans()
+        visualizer = KElbowVisualizer(model, k=(1, max_number_clusters + 1))
+        v = visualizer.fit(X)
+        v.show()
+        return KMeans(n_clusters=v.elbow_value_).fit(X), KMeans(n_clusters=v.elbow_value_).fit_predict(X)
+
+    ##HDBSCAN
+
+    def HDBSCAN(self, positions, n_clusters=None, affinity=None, max_number_clusters=None, cluster_size=1,
+                eps=None, min_samples=None, n_components=None):
+        X = self.get_reads_covering_positions_data(positions, plot=True)
+        return hdbscan.HDBSCAN(min_cluster_size=cluster_size, gen_min_span_tree=True).fit(X), hdbscan.HDBSCAN(
+            min_cluster_size=cluster_size, gen_min_span_tree=True).fit_predict(X)
+
+    ##DBSCAN
+
+    def DBSCAN_eps(self, positions):
         X = self.get_reads_covering_positions_data(positions)
         del X['read_id']
+        neigh = NearestNeighbors(n_neighbors=2)
+        nbrs = neigh.fit(X)
+        distances, indices = nbrs.kneighbors(X)
+        distances = np.sort(distances, axis=0)
+        distances = distances[:, 1]
+        a = plt.plot(distances)
+        plt.xlabel('Points')
+        plt.ylabel('eps value')
+        print(a)
+        i = np.arange(len(distances))
+        knee = KneeLocator(i, distances, S=1, curve='convex', direction='increasing', interp_method='polynomial')
+        fig = plt.figure()
+        knee.plot_knee()
+        plt.xlabel('Points')
+        plt.ylabel('eps')
+        print('Optimal eps value: ', distances[knee.knee])
 
-        tsne = TSNE(random_state=0)
+    def DBSCAN(self, positions, n_clusters=None, affinity=None, max_number_clusters=None, cluster_size=None,
+               eps=1, min_samples=1, n_components=None):
+        X = self.get_reads_covering_positions_data(positions, plot=True)
+        X = StandardScaler().fit_transform(X)
+        db = DBSCAN(eps=eps, min_samples=min_samples)
+        labels = db.fit(X).labels_
+        n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+        print('Estimated number of clusters: ', n_clusters_)
+        return db.fit(X), db.fit_predict(X)
+
+    ##Agglomerative clustering
+
+    def get_dendrogram(self, positions, y1=0, y2=0, y3=0):
+        X = self.get_reads_covering_positions_data(positions, plot=True)
+        hierarchy.dendrogram(linkage(X, method='single'), labels=X.index)
+        left, right = plt.xlim()
+        plt.hlines(y1, left, right, linestyles='dashed', label='y1')
+        plt.hlines(y2, left, right, linestyles='dashed', label='y2')
+        plt.hlines(y3, left, right, linestyles='dashed', label='y3')
+        plt.ylabel('Euclidean Distance')
+        plt.title('Dendrogram of subunit data')
+        plt.text(right, y1, 'y1')
+        plt.text(right, y2, 'y2')
+        plt.text(right, y3, 'y3')
+        return plt.show()
+
+    def agglomerative_clustering(self, positions, n_clusters=1, affinity=None, max_number_clusters=None,
+                                 cluster_size=None,
+                                 eps=None, min_samples=None, n_components=None):
+        X = self.get_reads_covering_positions_data(positions, plot=True)
+        return AgglomerativeClustering(n_clusters=n_clusters).fit(X), AgglomerativeClustering(
+            n_clusters=n_clusters).fit_predict(X)
+
+    ##affinity propagation
+
+    def affinity_propagation(self, positions, n_clusters=None, affinity=None, max_number_clusters=None,
+                             cluster_size=None, eps=None, min_samples=None, n_components=None):
+        X = self.get_reads_covering_positions_data(positions, plot=True)
+        return AffinityPropagation().fit(X), AffinityPropagation().fit_predict(X)
+
+    ##Gaussian mixture models
+
+    def gaussian_mixture_models_cluster(self, positions, n_clusters=None, affinity=None, max_number_clusters=None,
+                                        cluster_size=None, eps=None, min_samples=None, n_components=1):
+        X = self.get_reads_covering_positions_data(positions, plot=True)
+        return GaussianMixture(n_components=n_clusters).fit(X), GaussianMixture(n_components=n_clusters).fit_predict(X)
+
+    ##plot
+
+    def plot_tSNE_reads_covering_positions_data(self, positions, clustering_algorithm, n_clusters=2,
+                                                affinity='nearest_neighbors', max_number_clusters=10, cluster_size=10,
+                                                eps=2, min_samples=10, n_components=4, figure_path=None):
+        X = self.get_reads_covering_positions_data(positions, plot=True)
+        tsne = TSNE(n_components=2, random_state=0)
         tsne_results = tsne.fit_transform(X)
-        tsne_results = pd.DataFrame(tsne_results, columns=['tsne1', 'tsne2'])
-
-        if clustering_algorithm == 'GMM':
-            gmm = GaussianMixture(n_components=clusters_n).fit(X)
-            y_cluster = gmm.predict(X)
-            plt.scatter(tsne_results['tsne1'], tsne_results['tsne2'], c=y_cluster, s=30, cmap='rainbow')
-        #             print('Number of reads in each cluster: ', Counter(gmm.labels_))
-        if clustering_algorithm == 'KM':
-            kmeans = KMeans(n_clusters=clusters_n)
-            kmeans.fit(X)
-            y_cluster = kmeans.predict(X)
-            print('Number of reads in each cluster: ', Counter(kmeans.labels_))
-            plt.scatter(tsne_results['tsne1'], tsne_results['tsne2'], c=y_cluster, s=30, cmap='rainbow')
-        if clustering_algorithm == 'AP':      #doesn't need to specify clusters - fix longer code
-            afprop = AffinityPropagation(max_iter=250)
-            afprop.fit(X)
-            cluster_centers_indices = afprop.cluster_centers_indices_
-            n_clusters_ = len(cluster_centers_indices)
-            P = afprop.predict(X)
-            y_cluster = list(map(lambda x: '#3b4cc0' if x == 1 else '#b40426', P))
-            plt.scatter(tsne_results['tsne1'], tsne_results['tsne2'], c=y_cluster, s=30, cmap='rainbow')          
-        if clustering_algorithm == 'HDBSCAN':
-            clusterer = hdbscan.HDBSCAN(min_cluster_size=10, gen_min_span_tree=True)
-            clusterer.fit(X)
-            palette = sns.color_palette()
-            y_cluster = [sns.desaturate(palette[col], sat)
-                              if col >= 0 else (0.5, 0.5, 0.5) for col, sat in
-                              zip(clusterer.labels_, clusterer.probabilities_)]
-            plt.scatter(tsne_results['tsne1'], tsne_results['tsne2'], c=y_cluster, s=30, cmap='rainbow')
-        if clustering_algorithm == 'DBSCAN':   
-            ss = StandardScaler()
-            X = ss.fit_transform(X)
-            db = DBSCAN(eps=3, min_samples=10)
-            db.fit(X)
-            clusters = db.labels_
-            colors = ['royalblue', 'maroon', 'forestgreen', 'mediumorchid', 'tan', 'deeppink', 'olive', 'goldenrod', 'lightcyan', 'gray']
-            vectorizer = np.vectorize(lambda x: colors[x % len(colors)])
-            plt.scatter(tsne_results['tsne1'], tsne_results['tsne2'], c=vectorizer(clusters), s=30)
-        if clustering_algorithm == 'no':
-            y_cluster = temp_df.values
-
+        method_to_call = getattr(self, clustering_algorithm)
+        fit, predictor = method_to_call(positions, n_clusters, affinity, max_number_clusters, cluster_size, eps,
+                                        min_samples, n_components)
+        plt.scatter(tsne_results[:, 0], tsne_results[:, 1], c=predictor, s=30, cmap='rainbow')
         plt.xlabel("t-SNE 1")
         plt.ylabel("t-SNE 2")
-        plt.title(str(len(positions)) + ' ' + 'positions' + ' ' + clustering_algorithm + ' ' + 'clustering')
-
-        return plt.show()
+        plt.title(str(len(positions)) + ' ' + 'positions' + ' ' + clustering_algorithm)
+        if figure_path is not None:
+            assert not os.path.exists(figure_path), "Save fig path does exist: {}".format(figure_path)
+            plt.savefig(figure_path)
+        else:
+            plt.show()
+        return figure_path
 
     def plot_PCA_reads_covering_positions_data(self, positions, clusters_n, clustering_algorithm):
-        temp_df = self.get_reads_covering_positions_data(positions)
-        read_id = []
-        read_id = temp_df['read_id']
-        del temp_df['read_id']
-        scaler = StandardScaler()
-        scaler.fit(temp_df)
-        scaled_data = scaler.transform(temp_df.values)
-        pca = PCA(n_components=2)
-        pca.fit(scaled_data)
-        x_pca = pca.transform(scaled_data)
-        if clustering_algorithm == 'GMM':
-            gmm = GaussianMixture(n_components=clusters_n).fit(temp_df)
-            y_cluster = gmm.predict(temp_df)
-        if clustering_algorithm == 'KM':
-            kmeans = KMeans(n_clusters=clusters_n)
-            kmeans.fit(temp_df)
-            y_cluster = kmeans.predict(temp_df)
-        if clustering_algorithm == 'no':
-            y_cluster = temp_df.values
-        plt.scatter(x_pca[:, 0], x_pca[:, 1], s=40)
-        plt.scatter(x_pca[:, 0], x_pca[:, 1], c=y_cluster, s=40, cmap='rainbow')
-        plt.xlabel("PCA 1")
-        plt.ylabel("PCA 2")
-        plt.title(str(len(positions)) + ' ' + 'positions' + ' ' + clustering_algorithm + ' ' + 'clustering')
-        return plt.show()
+        pass
 
-    def plot_number_reads_covering_positions(self, contig, figure_path=None, verbose=False): 
+    def plot_number_reads_covering_positions(self, contig, figure_path=None, verbose=False):
         data = self.data.loc[self.data['contig'] == contig].set_index('reference_index')
-        data_2 = data.loc[:,'read_id']       
+        data_2 = data.loc[:, 'read_id']
         reads_dict = defaultdict(list)
         pos = []
         for index, value in data_2.items():
-            reads_dict[str(index)].append(value)  
+            reads_dict[str(index)].append(value)
             pos.append(index)
-        positions = list(dict.fromkeys(pos))    
+        positions = list(dict.fromkeys(pos))
         positions.reverse()
         n = 0
         d = {}
         for i in positions:
             n += 1
-            if n == 1:    
-                compare = reads_dict[str(i)]    
-                d[str(i)] = len(compare)
+            if n == 1:
+                comp = reads_dict[str(i)]
+                d[str(i)] = len(comp)
             else:
-                unique_val = set(reads_dict[str(i)]).intersection(compare)   
-                compare = list(unique_val)      
-                d[str(i)] = len(compare)
-        count_reads = pd.Series(d)        
-        count_reads.plot(title = 'Reads for ' + contig , legend = False)
+                unique_val = set(reads_dict[str(i)]).intersection(comp)
+                comp = list(unique_val)
+                d[str(i)] = len(comp)
+        count_reads = pd.Series(d)
+        count_reads.plot(title='Reads for ' + contig, legend=False)
         number = count_reads.min()
         if verbose:
             print('Reads covering all positions in ' + str(contig) + ': ' + str(number))
@@ -456,4 +565,3 @@ class VariantCall(object):
         else:
             plt.show()
         return figure_path, number
-    
