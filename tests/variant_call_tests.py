cdcfcb8e62ba687044d2754d866ea7a04fba0c26
Andrew D Bailey IV <bailey.andrew4@gmail.com>
2020-07-29 16:33:19 -0700

    Fix minor formatting issues in variant_call_tests

--- tests/variant_call_tests.py
+++ tests/variant_call_tests.py
@@ -26,8 +26,9 @@ class VariantCallTests(unittest.TestCase):
         cls.HOME = '/'.join(os.path.abspath(__file__).split("/")[:-2])
         cls.variant_call_file = os.path.join(cls.HOME, "tests/test_files/test_variant_call.csv")
         cls.vc = VariantCall(cls.variant_call_file)
-        cls.pos = [27,99,105,119,210,301,413,419,435,465,540,561,577,618,631,758,765,795,973,998,1006,1125,1180,1186,
-                   1190,1268,1270,1279,1289,1414,1427,1571,1574,1638,1772,1780,1781]
+        cls.pos = [27, 99, 105, 119, 210, 301, 413, 419, 435, 465, 540, 561, 577, 618, 631, 758, 765, 795, 973, 998,
+                   1006, 1125, 1180, 1186,
+                   1190, 1268, 1270, 1279, 1289, 1414, 1427, 1571, 1574, 1638, 1772, 1780, 1781]
 
     def test_variant_call_file(self):
         self.assertTrue(os.path.exists(self.variant_call_file))
@@ -97,87 +98,88 @@ class VariantCallTests(unittest.TestCase):
 
     def test_get_read_variant_data(self):
         id_1 = '03e6757b-31de-4b13-ab18-57f375404f28'
-        expected_1 = self.vc.data.iloc[[87,88,89,91,95,96,97,98,103,105,106,107,108,110,111,112,114,116,117,118,122,123,
-                                        124,126,127,129,130,134],:]
-        pd.testing.assert_frame_equal(expected_1, self.vc.get_read_variant_data(id_1,'T'))
+        expected_1 = self.vc.data.iloc[
+                     [87, 88, 89, 91, 95, 96, 97, 98, 103, 105, 106, 107, 108, 110, 111, 112, 114, 116, 117, 118, 122,
+                      123,
+                      124, 126, 127, 129, 130, 134], :]
+        pd.testing.assert_frame_equal(expected_1, self.vc.get_read_variant_data(id_1, 'T'))
 
         id_2 = '043a9f51-5127-4a29-bdfd-5154cf3fa3a7'
-        expected_2 = self.vc.data.iloc[[138,143,147,148], :]
+        expected_2 = self.vc.data.iloc[[138, 143, 147, 148], :]
         pd.testing.assert_frame_equal(expected_2, self.vc.get_read_variant_data(id_2, 'c'))
 
         id_3 = '02d2f886-87ff-4ab4-98f1-3eeb642f00c2'
-        expected_3 = self.vc.data.iloc[[323,324,325,326,330,335,336,337,340,343,344,349,350], :]
+        expected_3 = self.vc.data.iloc[[323, 324, 325, 326, 330, 335, 336, 337, 340, 343, 344, 349, 350], :]
         pd.testing.assert_frame_equal(expected_3, self.vc.get_read_variant_data(id_3, 'l'))
 
     def test_get_read_variant_set_data(self):
         id_1 = '03e6757b-31de-4b13-ab18-57f375404f28'
-        expected_1 = self.vc.data.iloc[[92,104,132,133], :]
+        expected_1 = self.vc.data.iloc[[92, 104, 132, 133], :]
         pd.testing.assert_frame_equal(expected_1, self.vc.get_read_variant_set_data(id_1, 'Cb'))
 
         id_2 = '043a9f51-5127-4a29-bdfd-5154cf3fa3a7'
-        expected_2 = self.vc.data.iloc[[152,153], :]
+        expected_2 = self.vc.data.iloc[[152, 153], :]
         pd.testing.assert_frame_equal(expected_2, self.vc.get_read_variant_set_data(id_2, 'Aj'))
 
         id_3 = '02d2f886-87ff-4ab4-98f1-3eeb642f00c2'
-        expected_3 = self.vc.data.iloc[[321,322,328,329,331,334,338,339], :]
+        expected_3 = self.vc.data.iloc[[321, 322, 328, 329, 331, 334, 338, 339], :]
         pd.testing.assert_frame_equal(expected_3, self.vc.get_read_variant_set_data(id_3, 'Aa'))
-
 
     def test_get_read_variants_data(self):
         id_1 = '03e6757b-31de-4b13-ab18-57f375404f28'
-        expected_1 = self.vc.data.iloc[[92,99,104,125,132,133], :]
-        pd.testing.assert_frame_equal(expected_1, self.vc.get_read_variants_data(id_1, ['C','e']))
+        expected_1 = self.vc.data.iloc[[92, 99, 104, 125, 132, 133], :]
+        pd.testing.assert_frame_equal(expected_1, self.vc.get_read_variants_data(id_1, ['C', 'e']))
 
         id_2 = '043a9f51-5127-4a29-bdfd-5154cf3fa3a7'
-        expected_2 = self.vc.data.iloc[[136,138,139,140,143,145,146,147,148,149,152,153], :]
+        expected_2 = self.vc.data.iloc[[136, 138, 139, 140, 143, 145, 146, 147, 148, 149, 152, 153], :]
         pd.testing.assert_frame_equal(expected_2.reset_index(drop=True),
-                                      self.vc.get_read_variants_data(id_2, ['G','k','l','j']).reset_index(drop=True))
+                                      self.vc.get_read_variants_data(id_2, ['G', 'k', 'l', 'j']).reset_index(drop=True))
 
         id_3 = '02d2f886-87ff-4ab4-98f1-3eeb642f00c2'
-        expected_3 = self.vc.data.iloc[[321, 322, 328, 329, 331, 334, 338, 339,356,357], :]
-        pd.testing.assert_frame_equal(expected_3, self.vc.get_read_variants_data(id_3, ['A','j']))
-
+        expected_3 = self.vc.data.iloc[[321, 322, 328, 329, 331, 334, 338, 339, 356, 357], :]
+        pd.testing.assert_frame_equal(expected_3, self.vc.get_read_variants_data(id_3, ['A', 'j']))
 
     def test_get_read_variant_sets_data(self):
         id_1 = '03e6757b-31de-4b13-ab18-57f375404f28'
-        expected_1 = self.vc.data.iloc[[87,99,111,112,116,117,125,127], :]
+        expected_1 = self.vc.data.iloc[[87, 99, 111, 112, 116, 117, 125, 127], :]
         pd.testing.assert_frame_equal(expected_1.reset_index(drop=True),
                                       self.vc.get_read_variant_sets_data(id_1, ['Td', 'Ce']).reset_index(drop=True))
 
         id_2 = '043a9f51-5127-4a29-bdfd-5154cf3fa3a7'
-        expected_2 = self.vc.data.iloc[[137,144,149,150,151,152,153], :]
-        pd.testing.assert_frame_equal(expected_2, self.vc.get_read_variant_sets_data(id_2, ['Gk', 'Ci','Cb','Aj']))
+        expected_2 = self.vc.data.iloc[[137, 144, 149, 150, 151, 152, 153], :]
+        pd.testing.assert_frame_equal(expected_2, self.vc.get_read_variant_sets_data(id_2, ['Gk', 'Ci', 'Cb', 'Aj']))
 
         id_3 = '02d2f886-87ff-4ab4-98f1-3eeb642f00c2'
-        expected_3 = self.vc.data.iloc[[327,341,345,354], :]
+        expected_3 = self.vc.data.iloc[[327, 341, 345, 354], :]
         pd.testing.assert_frame_equal(expected_3, self.vc.get_read_variant_sets_data(id_3, ['Tg', 'Cb']))
 
     def test_get_variant_sets(self):
         temp_df = pd.DataFrame({'variants': ['Aa', 'Tl', 'Cb', 'Gc', 'Tg', 'Td', 'Ci', 'Gk', 'Aj', 'Tdm', 'Th',
                                              'Ce', 'Af']})
-        assert_frame_equal(temp_df.reset_index(drop=True), (self.vc.get_variant_sets()).reset_index(drop=True), check_dtype = False)
-
+        assert_frame_equal(temp_df.reset_index(drop=True), (self.vc.get_variant_sets()).reset_index(drop=True),
+                           check_dtype=False)
 
     def test_get_number_of_variant_sets(self):
         self.assertEqual(13, self.vc.get_number_of_variant_sets())
 
     def test_get_variant_set_data(self):
-        temp_df = pd.DataFrame({'read_id' : ['02636c05-538f-4647-b3ec-8e3a8c5eb10e', '01196b69-900b-4dc0-95a7-169cd79fae9b',
-                                            '03e6757b-31de-4b13-ab18-57f375404f28', '04c78365-fd9f-4391-8ddb-277620028285',
-                                            '02d4da5c-ec95-43ac-ac61-9f98ef4a4ca1'],
-                                'contig': ['RDN25-1', 'RDN25-1', 'RDN25-1',
-                                'RDN25-1', 'RDN25-1'], 'reference_index': ['2346', '2346', '2346', '2346', '2346'],
-                                'strand': ['+', '+', '+', '+', '+'],
-                                'variants' : ['Tdm', 'Tdm', 'Tdm', 'Tdm', 'Tdm'],
-                                'prob1': ['0.902366', '0.722098', '0.739977', '0.794725', '0.840949'],
-                                'prob2' : ['0.048817',
-                                '0.138951', '0.130012', '0.102638', '0.079525'],
-                                'prob3': ['0.048817',
-                                '0.138951', '0.130012', '0.102638', '0.079525']})
+        temp_df = pd.DataFrame(
+            {'read_id': ['02636c05-538f-4647-b3ec-8e3a8c5eb10e', '01196b69-900b-4dc0-95a7-169cd79fae9b',
+                         '03e6757b-31de-4b13-ab18-57f375404f28', '04c78365-fd9f-4391-8ddb-277620028285',
+                         '02d4da5c-ec95-43ac-ac61-9f98ef4a4ca1'],
+             'contig': ['RDN25-1', 'RDN25-1', 'RDN25-1',
+                        'RDN25-1', 'RDN25-1'], 'reference_index': ['2346', '2346', '2346', '2346', '2346'],
+             'strand': ['+', '+', '+', '+', '+'],
+             'variants': ['Tdm', 'Tdm', 'Tdm', 'Tdm', 'Tdm'],
+             'prob1': ['0.902366', '0.722098', '0.739977', '0.794725', '0.840949'],
+             'prob2': ['0.048817',
+                       '0.138951', '0.130012', '0.102638', '0.079525'],
+             'prob3': ['0.048817',
+                       '0.138951', '0.130012', '0.102638', '0.079525']})
         temp_df = temp_df.astype({"reference_index": int, "prob1": float, "prob2": float, "prob3": float})
-        pd.testing.assert_frame_equal(temp_df.reset_index(drop=True), (self.vc.get_variant_set_data("Tdm")).reset_index(drop=True),
+        pd.testing.assert_frame_equal(temp_df.reset_index(drop=True),
+                                      (self.vc.get_variant_set_data("Tdm")).reset_index(drop=True),
                                       check_exact=False, check_less_precise=4)
-
 
     def test_get_positions_of_variant_set(self):
         temp_df = pd.DataFrame(
@@ -186,8 +188,9 @@ class VariantCallTests(unittest.TestCase):
              'strand': ['+'], 'variants': ['Tdm']})
 
         temp_df = temp_df.astype({"reference_index": int})
-        assert_frame_equal(temp_df.reset_index(drop = True), (self.vc.get_positions_of_variant_set("Tdm")).reset_index(drop = True),
-                           check_dtype= False)
+        assert_frame_equal(temp_df.reset_index(drop=True),
+                           (self.vc.get_positions_of_variant_set("Tdm")).reset_index(drop=True),
+                           check_dtype=False)
 
     def test_get_variant_sets_data(self):
         temp_df = pd.DataFrame(
@@ -196,7 +199,7 @@ class VariantCallTests(unittest.TestCase):
                          '043a9f51-5127-4a29-bdfd-5154cf3fa3a7', '031b1662-cbda-4efd-9120-257ac7b32eea',
                          '02c6037c-d73b-414d-9090-0bfe88a1e0b0', '04ac5ad4-d0d2-4bb4-bc8b-ff3b713661dc',
                          '02381d7b-ad58-4d21-8ee3-f77401c13814', '04c78365-fd9f-4391-8ddb-277620028285',
-                         '02d2f886-87ff-4ab4-98f1-3eeb642f00c2', '02d4da5c-ec95-43ac-ac61-9f98ef4a4ca1',],
+                         '02d2f886-87ff-4ab4-98f1-3eeb642f00c2', '02d4da5c-ec95-43ac-ac61-9f98ef4a4ca1', ],
              'contig': ['RDN18-1', 'RDN25-1', 'RDN25-1', 'RDN25-1', 'RDN18-1', 'RDN18-1', 'RDN18-1', 'RDN18-1',
                         'RDN18-1', 'RDN25-1', 'RDN18-1', 'RDN25-1'],
              'reference_index': ['1574', '2346', '2346', '2346', '1574', '1574', '1574', '1574', '1574', '2346', '1574',
@@ -207,21 +210,23 @@ class VariantCallTests(unittest.TestCase):
                        '0.215732', '0.794725', '1.000000', '0.840949'],
              'prob2': ['0.743411', '0.048817', '0.138951', '0.130012', '0.029482', '0.052882', '0.698256', '0.503365',
                        '0.784268', '0.102638', '0.000000', '0.079525'],
-             'prob3': ['NaN','0.048817', '0.138951', '0.130012', 'NaN', 'NaN', 'NaN', 'NaN', 'NaN', '0.102638',
+             'prob3': ['NaN', '0.048817', '0.138951', '0.130012', 'NaN', 'NaN', 'NaN', 'NaN', 'NaN', '0.102638',
                        'NaN', '0.079525']})
 
         temp_df = temp_df.astype({"reference_index": int, "prob1": float, "prob2": float, "prob3": float})
-        pd.testing.assert_frame_equal(temp_df.reset_index(drop=True), (self.vc.get_variant_sets_data(['Tdm', 'Gk'])).reset_index(drop=True),
-                           check_exact=False, check_less_precise=4)
+        pd.testing.assert_frame_equal(temp_df.reset_index(drop=True),
+                                      (self.vc.get_variant_sets_data(['Tdm', 'Gk'])).reset_index(drop=True),
+                                      check_exact=False, check_less_precise=4)
 
     def test_get_positions_of_variant_sets(self):
         temp_df = pd.DataFrame(
             {'contig': ['RDN18-1', 'RDN25-1'],
              'reference_index': ['1574', '2346'],
              'strand': ['+', '+'],
-             'variants': ['Gk','Tdm']})
+             'variants': ['Gk', 'Tdm']})
         temp_df = temp_df.astype({"reference_index": int})
-        assert_frame_equal(temp_df.reset_index(drop=True), (self.vc.get_positions_of_variant_sets(['Tdm', 'Gk'])).reset_index(drop=True),
+        assert_frame_equal(temp_df.reset_index(drop=True),
+                           (self.vc.get_positions_of_variant_sets(['Tdm', 'Gk'])).reset_index(drop=True),
                            check_dtype=False)
 
     def test_get_number_of_positions(self):
